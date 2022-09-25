2582e51958d5bc5f9babb307292d9ac8967634b1
Rhobar9 <86721432+Rhobar9@users.noreply.github.com>
2022-09-25 19:49:06 +0200

    Module4_attempt

--- src/services.js
+++ src/services.js
@@ -1,19 +1,27 @@
-export function fetchWithTimeout(delay){
+export function fetchWithTimeout(delay) {
     return new Promise(resolve => setTimeout(resolve, delay));
 }
 
-export function fetchMovies(){
+export function fetchMovies() {
 return fetch("./data/movies.json")
   .then(response => response.json())
   .then(movies => movies)
   .catch(error => console.log(error));
 }
 
-export function fetchBooks(){
+export function fetchBooks() {
 return fetch("./data/books.json")
   .then(response => response.json())
   .then(books => books)
   .catch(error => console.log(error));
 }
 
-
+export async function asyncFetchMovies() {
+    try {
+        const response = await fetch("./data/movies.json");
+        const results = await response.json();
+        return results;
+    } catch (error){
+        console.log(error);
+    }
+}
